13728b801f084958362d612745c9acc516743edc
amit-arya <amit3128arya@gmail.com>
2022-12-13 16:14:16 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
-    res.setHeader('Content-type','text/html');
-    res.write('<html>');
-    res.write('<body><h1>Welcome Home</h1></body>');
-    res.write('</html>');
-    res.end();
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Messege</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="messege"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if(url==='/message' && method ==='POST'){
+       const body = [];
+       req.on('data', (chunk) => {
+        body.push(chunk);
+       });
+       req.on('end',()=>{
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split('=')[1];
+        fs.writeFileSync('message.txt', message);
+       });
+
+       res.statusCode = 302;
+       res.setHeader('Location', '/');
+       return res.end();
+    }
+    // res.setHeader('Content-type','text/html');
+    // res.write('<html>');
+    // res.write('<body><h1>Welcome to my Node Js Project</h1></body>');
+    // res.write('</html>');
+    // res.end();
 });
 
 server.listen(4000);
