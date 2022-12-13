508ecd93d80902a94288ef42efd779236e8fe2d7
amit-arya <amit3128arya@gmail.com>
2022-12-13 21:52:04 +0530

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+  if (url === "/") {
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) console.log(err);
+
+      console.log("data from file : " + data);
+      res.write("<html>");
+      res.write("<head><title>Enter Messege</title></head>");
+      res.write(`<body>${data}</body>`);
+      res.write(
+        '<body><form action="/message" method="POST"><input type="text" name="messege"><button type="submit">Send</button></form></body>'
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  } else {
+    res.setHeader("Content-type", "text/html");
+    res.write("<html>");
+    res.write("<body><h1>Welcome to my Node Js Project</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
+};
+
+module.exports = requestHandler;
