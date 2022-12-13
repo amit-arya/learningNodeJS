f1dc6f20314ea144645facd3123ec1632d2acad7
amit-arya <amit3128arya@gmail.com>
2022-12-13 12:56:26 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    console.log('Amit Arya');
+    res.setHeader('Content-type','text/html');
+    res.write('<html>');
+    res.write('<body><h1>Welcome Home</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(4000);
