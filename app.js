d9a5da405d304ebdf166a06c1b4b296735bb5ab0
amit-arya <amit3128arya@gmail.com>
2022-12-13 17:50:47 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req, res)=>{
-    const url = req.url;
-    const method = req.method;
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
 
-    if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Enter Messege</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="messege"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
+  if (url === "/") {
+    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
+      if (err) console.log(err);
+
+      console.log("data from file : " + data);
+      res.write("<html>");
+      res.write("<head><title>Enter Messege</title></head>");
+      res.write(`<body>${data}</body>`);
+      res.write(
+        '<body><form action="/message" method="POST"><input type="text" name="messege"><button type="submit">Send</button></form></body>'
+      );
+      res.write("</html>");
+      return res.end();
+    });
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
         return res.end();
-    }
-
-    if(url==='/message' && method ==='POST'){
-       const body = [];
-       req.on('data', (chunk) => {
-        body.push(chunk);
-       });
-       req.on('end',()=>{
-        const parsedBody = Buffer.concat(body).toString();
-        const message = parsedBody.split('=')[1];
-        fs.writeFileSync('message.txt', message);
-       });
-
-       res.statusCode = 302;
-       res.setHeader('Location', '/');
-       return res.end();
-    }
-    // res.setHeader('Content-type','text/html');
-    // res.write('<html>');
-    // res.write('<body><h1>Welcome to my Node Js Project</h1></body>');
-    // res.write('</html>');
-    // res.end();
+      });
+    });
+  } else {
+    res.setHeader("Content-type", "text/html");
+    res.write("<html>");
+    res.write("<body><h1>Welcome to my Node Js Project</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
 });
 
 server.listen(4000);
-
