f5cd7f54ab6eb6d7da8bf295d01c2b48b25afe40
amit-arya <amit3128arya@gmail.com>
2022-12-14 17:15:10 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,45 +1,22 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+//const routes = require('./routes');
 
-  if (url === "/") {
-    fs.readFile("message.txt", { encoding: "utf-8" }, (err, data) => {
-      if (err) console.log(err);
+const express = require('express');
 
-      console.log("data from file : " + data);
-      res.write("<html>");
-      res.write("<head><title>Enter Messege</title></head>");
-      res.write(`<body>${data}</body>`);
-      res.write(
-        '<body><form action="/message" method="POST"><input type="text" name="messege"><button type="submit">Send</button></form></body>'
-      );
-      res.write("</html>");
-      return res.end();
-    });
-  } else if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  } else {
-    res.setHeader("Content-type", "text/html");
-    res.write("<html>");
-    res.write("<body><h1>Welcome to my Node Js Project</h1></body>");
-    res.write("</html>");
-    res.end();
-  }
-});
+const app = express();
+
+app.use((req, res, next)=> {
+    console.log('In the middleware');
+    next(); //allows the request to continue to the next middleware
+})
+
+app.use((req, res, next)=> {
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express</h1>'); // default header is text/html
+    // either write next or send a response 
+})
+
+const server = http.createServer(app);
 
 server.listen(4000);
