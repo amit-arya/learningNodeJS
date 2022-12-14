294b6b6f0d113212c8573886c7fc1fc352811f05
amit-arya <amit3128arya@gmail.com>
2022-12-14 18:55:08 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,20 +2,27 @@ const http = require("http");
 
 //const routes = require('./routes');
 
-const express = require('express');
+const express = require("express");
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next)=> {
-    console.log('In the middleware');
-    next(); //allows the request to continue to the next middleware
-})
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req, res, next)=> {
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express</h1>'); // default header is text/html
-    // either write next or send a response 
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.use('/product',(req, res, next)=> {
+    console.log(req.body);
+    res.redirect('/');
 })
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express</h1>"); // default header is text/html
+  // either write next or send a response
+});
 
 const server = http.createServer(app);
 
