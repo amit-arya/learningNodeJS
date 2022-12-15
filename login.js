e6ec641069804bc4faac25910bc30b96a82850d2
amit-arya <amit3128arya@gmail.com>
2022-12-15 13:47:45 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/login", (req, res, next) => {
+  res.send(
+    '<form onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)"action="/login" method="POST"><input type="text" id="username" placeholder="enter username"><button type="submit">Submit</button></form>'
+  );
+});
+
+router.post("/login", (req, res, next) => {
+  //console.log(req.body.username);
+  res.redirect("/");
+});
+
+module.exports = router;
