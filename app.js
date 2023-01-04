d2e8b60fb7c3d2ab54ee0c5a6c814b22f80a3016
amit-arya <amit3128arya@gmail.com>
2023-01-04 19:30:33 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
-const http = require("http");
+const path = require('path');
 
-//const routes = require('./routes');
-
-const express = require("express");
+const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>'
-  );
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next)=>{
+    User.findByPk(1)
+    .then(user=>{
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
 });
 
-app.post('/product',(req, res, next)=> {
-    console.log(req.body);
-    res.redirect('/');
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+Product.belongsTo(User, {constraints:true, onDelete:'CASCADE'});
+User.hasMany(Product);
+
+sequelize
+// .sync({ force:true })
+.sync()
+.then(result => {
+    return User.findByPk(1);
 })
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express</h1>"); // default header is text/html
-  // either write next or send a response
-});
+.then(user => {
+    if(!user){
+        return User.create({name:'Max', email:'test@test.com'});
+    }
+    return user;
+})
+.then(user => {
+    app.listen(3000);
+})
+.catch(err => {
+    console.log(err);
+})
 
-const server = http.createServer(app);
 
-server.listen(4000);
